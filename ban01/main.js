293ed332008dce25dbdbcfec6af7377acc8c32e6
YEJUN7 <56246666+YEJUN7@users.noreply.github.com>
2019-12-23 15:00:40 +0900

    Add files via upload

---
+++ ban01/main.js
@@ -0,0 +1,7 @@
+* { margin: 0; padding: 0; }
+    body, html { width: 100%; font-size:14px; font-family:'Sunflower', sans-serif;  }
+    html { overflow-x:hidden; }
+    body { overflow-y:auto; overflow-x:hidden; }
+    ul { list-style: none; }
+    a { text-decoration: none; }
+    label, input, button { border:0; outline:0; background-color:transparent; }
